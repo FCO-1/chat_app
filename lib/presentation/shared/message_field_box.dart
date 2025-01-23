669905e1c1d6919025c7060dd4a031eceb6c817a
FCO-1 <franc12345-67@hotmail.com>
2025-01-22 23:28:44 -0600

    cambios

--- lib/presentation/shared/message_field_box.dart
+++ lib/presentation/shared/message_field_box.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
+
   const MessageFieldBox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+  final textController = TextEditingController();
+  final focusNode = FocusNode();
+
+  final outlineborder = UnderlineInputBorder(
+    borderSide: const BorderSide(color: Colors.transparent),
+    borderRadius: BorderRadius.circular(40)
+  );
+
+  final inputDecoration  =   InputDecoration(
+    hintText: 'Tu mensaje termina con "?',
+        enabledBorder: outlineborder,
+        filled: true ,
+          suffixIcon:  IconButton(
+          onPressed:  () {
+             final textvalue = textController.value.text;
+            print('Valor registrado $textvalue');
+            textController.clear();
+          }, 
+          icon: const Icon( Icons.send_outlined)
+      ),
+      );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      controller: textController,
+      decoration: inputDecoration,
+      focusNode: focusNode,
+      onFieldSubmitted: (value) {
+        
+        print('Valor registrado $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+      
+    );
   }
 }
